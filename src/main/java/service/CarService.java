9053bcc0efacf0172505981681a10b5d32839b3e
Ayhan Krala <codewolf27@gmail.com>
2023-06-30 15:06:38 +0300

    adding files

--- src/main/java/service/CarService.java
+++ src/main/java/service/CarService.java
@@ -1,4 +1,31 @@
 package service;
 
+import entity.Car;
+import org.springframework.stereotype.Service;
+import repository.CarRepository;
+
+@Service
 public class CarService {
+    private CarRepository carRepository;
+
+    public CarService(CarRepository carRepository) {
+        this.carRepository = carRepository;
+    }
+
+    public Car getCarById(Long id) {
+        return carRepository.findById(id).orElse(null);
+    }
+
+    public void addCar(Car car) {
+        carRepository.save(car);
+    }
+
+    public void updateCar(Car car) {
+        carRepository.save(car);
+    }
+
+    public void deleteCar(Car car) {
+        carRepository.delete(car);
+    }
 }
+
