ea041829aa0f13f4c843630a425119349de85a2a
Ayhan Krala <codewolf27@gmail.com>
2023-06-30 15:04:33 +0300

    adding files

--- src/main/java/service/UserService.java
+++ src/main/java/service/UserService.java
@@ -1,4 +1,34 @@
 package service;
 
+import entity.User;
+import org.springframework.stereotype.Service;
+import repository.UserRepository;
+
+@Service
 public class UserService {
+    private UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public User getUserById(Long id) {
+        return userRepository.findById(id).orElse(null);
+    }
+
+    public User getUserByEmail(String email) {
+        return userRepository.findByEmail(email);
+    }
+
+    public void registerUser(User user) {
+        userRepository.save(user);
+    }
+
+    public void updateUser(User user) {
+        userRepository.save(user);
+    }
+
+    public void deleteUser(User user) {
+        userRepository.delete(user);
+    }
 }
