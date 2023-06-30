58f5d2e7b72286c151c87f52e858a42e241b3e04
Ayhan Krala <codewolf27@gmail.com>
2023-06-30 14:33:16 +0300

    adding files

---
+++ src/main/java/com/example/carrentalplatform/CarRentalPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.example.carrentalplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarRentalPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarRentalPlatformApplication.class, args);
+	}
+
+}
